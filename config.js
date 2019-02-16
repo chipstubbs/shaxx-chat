d184c0aea85b4c7a37c3be5e7c4ef827a78345f4
Chip Stubbs <cstubbs@advisorsacademy.com>
2019-02-15 21:40:07 -0500

    remove firebase config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyA-1o700EA5SOoStIgtu8jJmnAEF_me6GY",
-    authDomain: "shaxx-ec5ce.firebaseapp.com",
-    databaseURL: "https://shaxx-ec5ce.firebaseio.com",
-    projectId: "shaxx-ec5ce",
-    storageBucket: "shaxx-ec5ce.appspot.com",
-    messagingSenderId: "351096947652"
-};
